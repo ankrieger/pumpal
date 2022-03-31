a505010cfd8d2d3cc0c44cedb10b51cbe504f10a
Anastasia Krieger <anastasia0111@gmx.de>
2022-03-31 22:12:06 +0200

    Initial commit

---
+++ src/main/java/de/othr/sw/pumpal/PumpalApplication.java
@@ -0,0 +1,13 @@
+package de.othr.sw.pumpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/othr/sw/pumpal/PumpalApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.sw.pumpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
